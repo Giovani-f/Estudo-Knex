14f683dc99338f150a7527390920863006beb6ba
Giovani Fernandes <giovani@fieldcontrol.com.br>
2021-08-04 19:37:06 -0300

    crud basico

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const routes = express.Router()
 const UserControler = require('./controllers/UserController')
 
 routes.get('/users', UserControler.index)
+routes.post('/users', UserControler.create)
+routes.put('/users/:id', UserControler.update)
+routes.delete('/users/:id', UserControler.delete)
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -3,7 +3,20 @@ const routes = require('./routes')
 
 const app = express()
 
+app.use(express.json())
 app.use(routes)
 
+//not found
+app.use((req, res, next) => {
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
+
+//catch all
+app.use((err, req, res, next) => {
+  res.status(err.send || 500)
+  res.json({ error: err.message })
+})
 
 app.listen(3000, () => {console.log('Server is running in port 3000...')})
